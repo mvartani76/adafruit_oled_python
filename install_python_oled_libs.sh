14e855fd07f023d74c23f36fcbb5c4f3e693b60c
mvartani76 <mike.vartanian@gmail.com>
2019-01-15 12:05:44 -0500

    Added python library install at start of script.

--- install_python_oled_libs.sh
+++ install_python_oled_libs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Install necessary Python Libraries just in case
+sudo apt-get install -y python-dev
+sudo apt-get install -y python-imaging python-smbus i2c-tools
+
 # Navigate to Adafruit Python GPIO directory and install module
 cd Adafruit_Python_GPIO
 sudo python setup.py install
